99b63fb541ae132ca2168851c8b855a2d376ef8f
Patrick <patrickrebecchi@hotmail.com>
2025-03-27 20:32:48 -0300

    Adiciona validações (Data Annotaions)  para as entidades Produto e Categoria

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,17 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+[Table("Produtos")]
 public class Produto
 {
     [Key]
     public int ProdutoId { get; set; }
+    [Required(ErrorMessage ="Valor invalido")]
     public string? Nome { get; set; }
+    [Required(ErrorMessage = "Valor invalido")]
     public string? Descricao { get; set; }
+    [Required(ErrorMessage = "Valor invalido")]
+    [Column(TypeName = "decimal(10, 2)")]
     public decimal Preco { get; set; }
+    [Required(ErrorMessage = "Valor invalido")]
     public string? ImagemUrl { get; set; }
+    [Required(ErrorMessage = "Valor invalido")]
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
     public int CategoriaId { get; set; }
-    public Categoria Categoria { get; set; }
+    public Categoria? Categoria { get; set; }
 }
