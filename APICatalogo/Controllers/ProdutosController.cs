8e66511281f30ed4fafeee829b901bb777b8e83a
Patrick <patrickrebecchi@hotmail.com>
2025-04-02 22:24:58 -0300

    Endpoints, criações

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -9,13 +9,33 @@ namespace APICatalogo.Controllers
     [ApiController] // Define que a classe é um controller de API
     public class ProdutosController : ControllerBase
     {
+        private readonly AppDbContext _context; // Contexto do banco de dados
+
+        public ProdutosController(AppDbContext context) 
+        {
+            _context = context;
+        }
+
         // Endpoint GET para retornar todos os produtos
         [HttpGet]
-        public async Task<IEnumerable<Produto>> GetAsync(
-            [FromServices] AppDbContext context)
+        public async Task<IEnumerable<Produto>> GetAsync() // Retorna uma lista de produtos
         {
             // Retorna todos os produtos da tabela 'Produtos'
-            return await context.Produtos.ToListAsync();
+            return await _context.Produtos.ToListAsync();
         }
+
+        [HttpGet("{id}")] // Define um parâmetro na rota. Exemplo: /produtos/1
+        public async Task<ActionResult<Produto>> GetAsync(int id)
+        {
+            // Busca um produto pelo ID
+            var produto = await _context.Produtos.FindAsync(id);
+            // Se não encontrar o produto, retorna 404
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            // Retorna o produto encontrado
+            return produto;
+        } 
     }
 }
